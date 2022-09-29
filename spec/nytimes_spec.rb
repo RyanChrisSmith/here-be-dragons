1c7306848095d85bc9c9f36d663222e9a5eb09ee
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-29 13:39:43 -0600

    tests passing

--- spec/nytimes_spec.rb
+++ spec/nytimes_spec.rb
@@ -15,23 +15,27 @@ class Nytimes
       expect(result).to eq("OK")
     end
 
-    xit 'can get copyright' do 
+    it 'can get copyright' do
+      result = @hash[:copyright]
       #Using @hash, define a variable called `result` that returns the copyright
 
       expect(result).to eq("Copyright (c) 2018 The New York Times Company. All Rights Reserved.")
     end
 
-    xit 'can get array of stories' do 
+    it 'can get array of stories' do
+      result = @hash[:results]
       #Using @hash, define a variable called `result` that returns the array of stories
   
-      expect(result.class).to be_a(Array)
+      expect(result.class).to eq(Array)
       expect(result.count).to eq(44)
     end
 
-    xit 'can get all stories with subsection of politics' do 
-      #Using @hash, define a variable called `result` that returns all stories with subsection of politics.
-  
-      expect(result.class).to be_a(Array)
+    it 'can get all stories with subsection of politics' do
+      result = @hash[:results].find_all do |each_result|
+        each_result[:subsection].include?("Politics")
+      end
+        #Using @hash, define a variable called `result` that returns all stories with subsection of politics.
+      expect(result.class).to eq(Array)
       expect(result.count).to eq(6)
       expect(result.first[:title]).to eq("Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight")
       expect(result.last[:title]).to eq("Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.")
