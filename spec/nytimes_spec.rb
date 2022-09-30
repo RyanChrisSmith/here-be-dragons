31cfa6153b85b0713a92dfe4f435ee49e7d41904
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-30 13:18:02 -0600

    updated tests for array

--- spec/nytimes_spec.rb
+++ spec/nytimes_spec.rb
@@ -26,7 +26,7 @@ class Nytimes
       result = @hash[:results]
       #Using @hash, define a variable called `result` that returns the array of stories
   
-      expect(result.class).to eq(Array)
+      expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(44)
     end
 
@@ -35,7 +35,7 @@ class Nytimes
         each_result[:subsection].include?("Politics")
       end
         #Using @hash, define a variable called `result` that returns all stories with subsection of politics.
-      expect(result.class).to eq(Array)
+      expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(6)
       expect(result.first[:title]).to eq("Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight")
       expect(result.last[:title]).to eq("Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.")
